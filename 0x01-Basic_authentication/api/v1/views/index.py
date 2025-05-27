eef07e657b7de4094374b34b1ed415866ee7ea4d
owolabenjade <owolabenjade@gmail.com>
2025-05-27 16:58:19 +0100

    Basic

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,13 @@ def unauthorized() -> str:
       - 401 error
     """
     abort(401)
+
+
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def forbidden() -> str:
+    """
+    GET /api/v1/forbidden
+    Raises:
+      - 403 error
+    """
+    abort(403)
