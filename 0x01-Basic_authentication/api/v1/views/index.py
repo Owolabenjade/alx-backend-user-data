bef9ac44d04b61db0bb31851e63ac38446c6a83b
owolabenjade <owolabenjade@gmail.com>
2025-05-27 16:53:18 +0100

    Basic_authentication

---
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+Index file
+"""
+from flask import jsonify, abort
+from api.v1.views import app_views
+
+
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
+def status() -> str:
+    """
+    GET /api/v1/status
+    Return:
+      - the status of the API
+    """
+    return jsonify({"status": "OK"})
+
+
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
+def unauthorized() -> str:
+    """
+    GET /api/v1/unauthorized
+    Raises:
+      - 401 error
+    """
+    abort(401)
